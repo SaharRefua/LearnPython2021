4782af0a102d912bd2a4d1ac0c1dc88681d806c7
saharrefua <sahar.refua@maxq.ai>
2021-12-26 10:13:04 +0200

    Add GET API Script

--- API/API_test.py
+++ API/API_test.py
@@ -2,16 +2,16 @@ import requests
 import json
 resposnse =requests.get("https://api.stackoverflow.com/2.3/questions/")
 print(resposnse)
-resposnse.raise_for_status()  # raises exception when not a 2xx response
+#resposnse.raise_for_status()#
+#
+# if resposnse.status_code != 204:
+#     with open('resposnse.json', 'a') as f:
+#         f.write(str(resposnse.content))
 
-if resposnse.status_code != 204:
-    with open('API/resposnse.json', 'w') as f:
-        f.write(str(resposnse.content))
 
-
-   # if resposnse.status_code != 204:
+#   if resposnse.status_code != 204:
 #    resposnse.decode('utf-8').replace('\0', '')
-# for data in resposnse.json()['items']:
-#     print(data)
+for data in resposnse.json():#['items']:
+    print(data)
 # open('data.json', 'a') as file:
 #     file.write(resposnse)
