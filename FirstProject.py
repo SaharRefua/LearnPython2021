a4e3a8ec516d7324e576ccfafb6f617e6d8a40d4
saharrefua <sahar.refua@maxq.ai>
2021-12-29 22:29:13 +0200

    Update FirstProject.py script

--- FirstProject.py
+++ FirstProject.py
@@ -18,29 +18,50 @@ def account_status ():
     pass
 
 
-def Deposit(Deposit_value):
+def withdraw(withdraw_value):
     f = open('data.json')
     data = json.load(f)
-
     value = data["Balance"]
-    value=float(value)+float(Deposit_value)
+    value = float(value) - float(withdraw_value)
+    data["Balance"] = str(value)
+    json.dumps(data)
     f.close()
     print(f"Your Balance in the bank Account is {value}\n")
 
+def close_account():
+    f = open('data.json')
+    data = json.load(f)
+    value = data["active"]
+    value = False
+    data["Balance"] = str(value)
+    json.dumps(data)
+    f.close()
+    print(f"Your accout was closed\n")
 
+
+def deposit(deposit_value):
+    f = open('data.json')
+    data = json.load(f)
+    value = data["Balance"]
+    value=float(value)+float(deposit_value)
+    data["Balance"]=str(value)
+    json.dumps(data)
+    f.close()
+    print(f"Your Balance in the bank Account is {value}\n")
 
 def account_actions():
-    sahar_answer=input("\nPlease let us know what are the action that you what to do in your account:\n 1.Deposit money to your account \n 2.Withdraw money from your account \n 3.Close your account")
-    if sahar_answer==1:
-        Deposit_value = input("How match money do you what to Deposit in your account?\n")
-        Deposit(Deposit_value)
-    # elif: sahar_answer == 2:
-    #
-    #
-    # else: sahar_answer == 3:
+    sahar_answer=input("\nPlease let us know what are the action that you what to do in your account:\n 1.Deposit money to your account \n 2.Withdraw money from your account \n 3.Close your account\n")
+    if sahar_answer=="1":
+        deposit_value = input("How match money do you what to Deposit in your account?\n")
+        deposit(deposit_value)
+    elif sahar_answer == "2":
+        withdraw_value = input("How match money do you what to Deposit in your account?\n")
+        withdraw(withdraw_value)
+    elif sahar_answer== "3":
+        close_account()
     else:
         print("Wrong action please try again")
-
+        account_actions()
 
 def upload_accout_data():
     f = open('data.json')
@@ -49,11 +70,8 @@ def upload_accout_data():
     value=data["Balance"]
     print(f"Your Balance in the bank Account is {value}\n")
 
-
-print("Welcome to Sahar Bank accout\n")
+#print("Welcome to Sahar Bank accout\n")
 #log_in()
 # if login_successfully==True:
 #      upload_accout_data()
 account_actions()
-
-
