bbd178bc2fb7cfde4920aa4a1f579f47ae3de05d
Sahar Refua <refua.sahar@gmail.com>
2022-01-28 17:05:49 +0200

    Update create folder script

--- 100DaysOfPythonProBootcampFor2022/Day-0/CreateForlders.py
+++ 100DaysOfPythonProBootcampFor2022/Day-0/CreateForlders.py
@@ -1,11 +1,11 @@
 import os
 folders_path = "C:/PythonProjects/LearnPython2021/100DaysOfPythonProBootcampFor2022" #input("Ender folder dirctory:")
-folders_name = input("Ender folder name:")
-folders_number = int(input("Ender folders number that you what to create :"))
+folders_name ="Day-" #input("Enter folder name:")
+folders_number = int(input("Enter folders number that you what to create :"))
 os.chdir(folders_path)
 
 for i in range (folders_number):
-    try :
+    try:
         os.mkdir(f"{folders_name}{i}")
     except:
         print(f"Can't create folder{folders_name}{i}")
